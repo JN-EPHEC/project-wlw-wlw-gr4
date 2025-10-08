ce1948d234d2ab01af530fed25ede63dbd20a898
nathanmoitie <he135473@students.ephec.be>
2025-10-08 11:54:04 +0200

    changement 1.0

---
+++ components/nathan.tsx
@@ -0,0 +1,23 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function CoucouComponent() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Coucou</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center', // centre verticalement
+    alignItems: 'center', // centre horizontalement
+    backgroundColor: '#f5f5f5',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#333',
+  },
+});
