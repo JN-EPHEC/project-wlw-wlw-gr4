a5b474b44522b2fa46f54967798c1b0d8a9b1dd7
Anthony-Krupic <anthony.krp25@gmail.com>
2025-10-08 12:05:57 +0200

    jcjf

---
+++ components/Anthony.tsx
@@ -0,0 +1,29 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function HelloWorld() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>👋 Hello React Native!</Text>
+      <Text style={styles.subtitle}>I’m ready to build cool mobile apps 🚀</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#f0f8ff",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#333",
+    marginBottom: 10,
+  },
+  subtitle: {
+    fontSize: 16,
+    color: "#555",
+  },
+});
