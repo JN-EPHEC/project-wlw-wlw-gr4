dabaedd068a56867f7a77f992dfedddb478eba2f
Anthony-Krupic <anthony.krp25@gmail.com>
2025-10-08 12:18:19 +0200

    new changes

--- components/Anthony.tsx
+++ components/Anthony.tsx
@@ -1,9 +1,10 @@
+import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function HelloWorld() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>👋 salut les brothers !</Text>
+      <Text style={styles.title}>👋 Hello React Native!</Text>
       <Text style={styles.subtitle}>I’m ready to build cool mobile apps 🚀</Text>
     </View>
   );
